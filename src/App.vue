f460d4a10ca3f74221324eb5c41f127b35d6258a
Fabio Carpinato <fabiocarpinato@outlook.it>
2022-05-21 15:32:04 +0200

    adds tailwind and base page structure

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,11 @@
-<template>Hello World</template>
+<template>
+  <div class="flex w-screen h-screen text-gray-700">
+    <div
+      class="flex flex-col flex-shrink-0 w-64 border-r border-gray-300 bg-gray-100"
+    ></div>
+    <div class="flex flex-col flex-grow"></div>
+  </div>
+</template>
 
 <script lang="ts">
 import { Options, Vue } from "vue-class-component";
