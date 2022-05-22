e082454ef0849c12e8d60c122c082a842ad8ea83
Fabio Carpinato <fabiocarpinato@outlook.it>
2022-05-22 13:25:57 +0200

    saves and retrieves notes from indexed db

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,26 @@
   <div class="flex w-screen h-screen text-gray-700">
     <div
       class="flex flex-col flex-shrink-0 w-64 border-r border-gray-300 bg-gray-100"
-    ></div>
+    >
+      <div class="h-0 overflow-auto flex-grow">
+        <div class="mt-4">
+          <a
+            class="flex items-center h-8 text-sm pl-8 pr-3"
+            href="#"
+            v-for="note in notes"
+            :key="note.id"
+            ><span class="ml-2 leading-none">{{
+              new Date(note.created).toLocaleString()
+            }}</span></a
+          >
+        </div>
+      </div>
+    </div>
     <div class="flex flex-col flex-grow">
       <tiptap></tiptap>
+      <div class="h-16 bg-gray-100 border-t border-gray-300 text-right">
+        <button @click="saveNote">Save Note</button>
+      </div>
     </div>
   </div>
 </template>
@@ -17,5 +34,72 @@ import Tiptap from "./components/Tiptap.vue";
 @Options({
   components: { Tiptap },
 })
-export default class App extends Vue {}
+export default class App extends Vue {
+  database: IDBDatabase | null = null;
+
+  notes: any[] = [];
+
+  async created() {
+    this.database = (await this.getDatabase()) as IDBDatabase;
+
+    this.notes = ((await this.getNotes()) as any[]).sort(
+      (a, b) => new Date(b.created).getTime() - new Date(a.created).getTime()
+    );
+  }
+
+  async getDatabase() {
+    return new Promise((resolve, reject) => {
+      const db = window.indexedDB.open("offline_notes");
+
+      db.onerror = () => {
+        reject("Error opening database");
+      };
+
+      db.onsuccess = (e: any) => {
+        resolve(e.target.result);
+      };
+
+      db.onupgradeneeded = (e: any) => {
+        e.target.result.deleteObjectStore("notes");
+        e.target.result.createObjectStore("notes", {
+          keyPath: "id",
+          autoIncrement: true,
+        });
+      };
+    });
+  }
+
+  async saveNote() {
+    return new Promise((resolve, reject) => {
+      let transaction = this.database?.transaction("notes", "readwrite");
+
+      if (!transaction) {
+        reject("Error creating the transaction");
+        return;
+      }
+
+      transaction.oncomplete = (e: any) => {
+        resolve(e.target.result);
+      };
+
+      const note = {
+        content: "test",
+        created: new Date().getTime(),
+      };
+
+      this.notes.unshift(note);
+      transaction
+        .objectStore("notes")
+        .add(note, Math.round(Math.random() * 1000000));
+    });
+  }
+
+  async getNotes() {
+    return new Promise((resolve, reject) => {
+      this.database!.transaction("notes", "readonly")
+        .objectStore("notes")
+        .getAll().onsuccess = (e: any) => resolve(e.target.result);
+    });
+  }
+}
 </script>
